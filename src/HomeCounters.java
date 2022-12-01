2d3cee55e49e42a55f47d723d249f8ce5cf28eee
etereshhenko <e.tereshhenko@cft.ru>
2022-12-02 00:18:18 +0700

    all

--- src/HomeCounters.java
+++ src/HomeCounters.java
@@ -1,5 +1,25 @@
 public class HomeCounters {
     public static void main(String[] args) {
+        double couldWater = 49.58;
+        double hotWater = 138.01;
+        double waterDisposal = 33.4;
+        double electricity = 2.77;
+
+        int countCouldWater = 12;
+        int countHotWater = 10;
+        int countElectricity = 250;
+
+        double countWater = (countCouldWater * couldWater) + (countHotWater * hotWater) + ((countCouldWater + countHotWater) * waterDisposal );
+        double countPower = electricity * countElectricity;
+        double totalPrice = countWater + countPower;
+        double tp = totalPrice;
+        tp = Math.round(tp * 100);
+        tp = tp/100;
+
+        System.out.println("Ваши расходы по счетчикам " + totalPrice);
+        System.out.println("Ваши расходы по счетчикам " + tp);
+
+
 
     }
 }
